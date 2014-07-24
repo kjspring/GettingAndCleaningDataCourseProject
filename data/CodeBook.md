7c621b4bb878cafc0aa42b25c384050240e6dd01
kjspring <kevinjspring@gmail.com>
2014-07-24 18:35:32 -0500

    updating CodeBook.md

---
+++ data/CodeBook.md
@@ -0,0 +1,38 @@
+# Tidy Data Creation of Human Activity Recognition Using Smartphones Data Set Code Book
+
+The purpose of this data analysis is to take the raw data from the Human Activity Recognition Using Smartphones Data Set and turn it into a usable tidy data set.
+
+## Requirements
+* Linux or Apple OSX
+* R version 3.0.2 for Linux or R version 3.1.1 for OSX
+
+# Important Files
+
+* README.md
+* run_analysis.R
+* data/CodeBook.md
+* data/raw/activity_labels.txt
+* data/raw/features.txt
+* data/raw/features_info.txt
+* data/raw/README.txt
+* data/raw/test/subject_test.txt
+* data/raw/test/X_test.txt
+* data/raw/test/y_test.txt
+
+## User Steps
+
+1. Copy the data and script from Github (https://github.com/kjspring/GettingAndCleaningDataCourseProject)
+2. Open R or RStudio
+3. Change the working directory to the directory that contains the run_analysis.R script
+4. Run the run_analysis.R script in R or RStudio.
+
+# Data Analysis Steps
+
+The file run_analysis.R completes all the data manimpulation and analysis steps.
+
+
+Directions:
+
+Describes the variables, the data, and any transformations or work that you performed to clean up the data.
+
+The
--- run_analysis.R
+++ run_analysis.R
@@ -10,22 +10,22 @@
 rm(list=ls()) # clear environment workspace
 
 # Merge all the training data into one datatable
-subject_train <- read.table("data/UCI HAR Dataset/train/subject_train.txt", header=F) # subject
-X_train <- read.table("data/UCI HAR Dataset/train/X_train.txt", header=F) # freq var
-Y_train <- read.table("data/UCI HAR Dataset/train/y_train.txt", header=F) # activity
+subject_train <- read.table("data/raw/train/subject_train.txt", header=F) # subject
+X_train <- read.table("data/raw/train/X_train.txt", header=F) # freq var
+Y_train <- read.table("data/raw/train/y_train.txt", header=F) # activity
 train <- cbind(subject_train, Y_train, X_train)
 
 # Merge all the test data into one datatable
-subject_test <- read.table("data/UCI HAR Dataset/test/subject_test.txt", header=F) # subject
-X_test <- read.table("data/UCI HAR Dataset/test/X_test.txt", header=F) # freq var
-Y_test <- read.table("data/UCI HAR Dataset/test/y_test.txt", header=F) # activity
+subject_test <- read.table("data/raw/test/subject_test.txt", header=F) # subject
+X_test <- read.table("data/raw/test/X_test.txt", header=F) # freq var
+Y_test <- read.table("data/raw/test/y_test.txt", header=F) # activity
 test <- cbind(subject_test, Y_test, X_test)
 
 # Combine train and test data
 combinedDat <- rbind(test, train)
 
 # Name the columns in combinedDat with descriptive names
-features <- read.table("data/UCI HAR Dataset/features.txt", header=F)
+features <- read.table("data/raw/features.txt", header=F)
 features[,2] <- as.character(features[,2])
 colnames(combinedDat) <- c("subject", "activity", features[,2])
 
@@ -39,7 +39,7 @@ meanSDdat <- subset(combinedDat, select = c(1, 2, meanSDcol))
 # Exchange the activity code for actual descriptive variables
 
 # loads the activity code map
-activityLabels <- read.table("data/UCI HAR Dataset/activity_labels.txt", 
+activityLabels <- read.table("data/raw/activity_labels.txt", 
                              stringsAsFactors=F) 
                                     
 ########################################################################
