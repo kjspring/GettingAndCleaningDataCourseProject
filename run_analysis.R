39203d929aa2c4cb84dc4e2bdb277278a0f638f1
kjspring <kevinjspring@gmail.com>
2014-07-24 17:48:06 -0500

    added more comments in the run_analysis.R script

--- run_analysis.R
+++ run_analysis.R
@@ -1,12 +1,15 @@
-
-
-
+# Title: Tidy Data Maker for Human Activity Recognition Using Smartphones Data 
+# Set 
+# Author: Kevin Spring
+# Date: 23 July 2014
+# https://class.coursera.org/getdata-005
+###############################################################################
+# See the README.MD for information on how the raw data is stored 
+# in the file structure
 
 rm(list=ls()) # clear environment workspace
-# See the README.MD for information on how the raw data is stored in the file structure
 
-## Merge all the training data into one datatable
-
+# Merge all the training data into one datatable
 subject_train <- read.table("data/UCI HAR Dataset/train/subject_train.txt", header=F) # subject
 X_train <- read.table("data/UCI HAR Dataset/train/X_train.txt", header=F) # freq var
 Y_train <- read.table("data/UCI HAR Dataset/train/y_train.txt", header=F) # activity
@@ -21,22 +24,24 @@ test <- cbind(subject_test, Y_test, X_test)
 # Combine train and test data
 combinedDat <- rbind(test, train)
 
-# Name the columns in combinedDat
+# Name the columns in combinedDat with descriptive names
 features <- read.table("data/UCI HAR Dataset/features.txt", header=F)
 features[,2] <- as.character(features[,2])
 colnames(combinedDat) <- c("subject", "activity", features[,2])
 
-# Extracts only the measurements on the mean and standard deviation for each measurement
-
-## Find which columns contain within the header names std() or mean()
+# Extracts only the measurements on the mean and standard
+# deviation for each measurement
 meanSDcol <- grep("[sS]td|[mM]ean", names(combinedDat))
+
+# Store only the mean and standard deviation values in a new data frame
 meanSDdat <- subset(combinedDat, select = c(1, 2, meanSDcol))
 
-# Uses descriptive activity names to name the activities in the data set
-## In this step we want to substitute the numeric code with the actual description
+# Exchange the activity code for actual descriptive variables
 
-activityLabels <- read.table("data/UCI HAR Dataset/activity_labels.txt", stringsAsFactors=F)
-
+# loads the activity code map
+activityLabels <- read.table("data/UCI HAR Dataset/activity_labels.txt", 
+                             stringsAsFactors=F) 
+                                    
 ########################################################################
 # Function subLabels
 # Substitutes activity code for description of activity to follow
@@ -51,10 +56,13 @@ subLabels <- function (inputDF, mapDF) {
 # end function
 #########################################################################
 
+# Call the subLabels function to create tidy data on the combined
+# data sets and on the data sets that contain only the mean and 
+# standard deviation values
 tidyDat <- subLabels(combinedDat, activityLabels)
 tidyDat_meanSD <- subLabels(meanSDdat, activityLabels)
 
-# Save tidy data
+# Save tidy data to the data/tidy folder
 if(!file.exists("data/tidy")) {
   dir.create("data/tidy")
 }
--- scripts/README.md
+++
@@ -1,5 +0,0 @@
-# Getting and Cleaning Data Course Project
-This repo explains how all of the scripts work and how they are connected.
-
-## File list
-
